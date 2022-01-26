2a10fb076cfdd0a5b0e6bab4229813e580ac821c
Johan27 <30929823+Johansuted27@users.noreply.github.com>
2022-01-26 19:46:11 +0700

    upload vue

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <Header />
+    <Hero />
+    <WomenBanner />
+    <Instagram />
+    <PartnerLogo />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue'
+import Hero from '@/components/Hero.vue'
+import WomenBanner from '@/components/WomenBanner.vue'
+import Instagram from '@/components/Instagram.vue'
+import PartnerLogo from '@/components/PartnerLogo.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    Hero,
+    WomenBanner,
+    Instagram,
+    PartnerLogo,
+    Footer
+  }
+}
+</script>
